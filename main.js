7cd43f2677d08e9b80dfd50d6547858921a8f9aa
papito_chulo <bohemiaverdealux@gmail.com>
2024-06-12 16:28:05 -0600

    PRIMERCOMMIT

---
+++ main.js
@@ -0,0 +1,10 @@
+var nombre = "Christiansapon";
+var altura = 100;
+var concatenacion = nombre + " " + altura;
+
+document.write(concatenacion);
+
+var datos= document.getElementById("datos");
+datos.innerHTML - concatenacion;
+
+
